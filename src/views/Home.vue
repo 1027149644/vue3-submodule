0f29e1009a128bbd5aad1650928dc949eae24980
1027149644@qq.com <1027149644@qq.com>
2019-08-13 13:45:02 +0800

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <k-header></k-header>
+        <router-view />
+        <k-footer></k-footer>
+    </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+import { Header, Footer } from '@comps'
+export default {
+    components: {
+        'k-header': Header,
+        'k-footer': Footer
+    },
+    computed: {
+        //vuex order命名空间
+         ...mapState('order', ['orders'])
+    },
+    mounted() {
+        console.log(this.orders)
+    }
+}
+</script>
+
---
+++ src/views/admin/pages/address.vue
@@ -0,0 +1,10 @@
+<script>
+/**
+ * 地址管理
+ */
+</script>
+<template>
+    <div>
+        北京市丰台区首经贸
+    </div>
+</template>
